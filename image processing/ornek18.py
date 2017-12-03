4225db1ba3a3e1144c23dab53cd4fb99e73be913
Nevzat BOL <33426517+NevzatBOL@users.noreply.github.com>
2017-12-03 18:38:48 +0300

    Update ornek18.py

--- image processing/ornek18.py
+++ image processing/ornek18.py
@@ -6,9 +6,9 @@ import cv2
 from PIL import Image
 import pytesseract
 
-img=cv2.imread('text2.jpg')
+img=cv2.imread('resimler/text2.jpg')
 gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-thres=cv2.threshold(gray,0,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+thres=cv2.threshold(gresimler/ray,0,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 #gray = cv2.medianBlur(gray, 3)
 
 #lang ile farklı diller için resimden text çekilebilir.
